530307f6f2fe0ccbb14d15a5e57c78b11397eb69
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-05-07 20:01:19 +0700

    Tambah fitur highlight pencarian

--- js/script.js
+++ js/script.js
@@ -23,12 +23,23 @@ fetchData().then(users => {
         const startIndex = (currentPage - 1) * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
         const visibleItems = filteredItems.slice(startIndex, endIndex);
-        searchList.innerHTML = filteredItems.map(item => `<li>${item}</li>`).join("");
-    }
+        searchList.innerHTML = visibleItems.map(item => `<li>${highlightSearch(item, searchValue)}</li>`).join("");
+    }    
 
     searchInput.addEventListener("input", updateSearchList);
     updateSearchList();
 })
+
+function highlightSearch(text, searchValue) {
+    const index = text.toLowerCase().indexOf(searchValue);
+    if (index === -1) {
+        return text;
+    }
+    const beforeMatch = text.slice(0, index);
+    const match = text.slice(index, index + searchValue.length);
+    const afterMatch = text.slice(index + searchValue.length);
+    return `${beforeMatch}<span class="highlight">${match}</span>${afterMatch}`;
+}
 
 window.addEventListener("scroll", () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
